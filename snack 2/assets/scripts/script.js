e45de0155ae87f8c17bf270023e2fb6090e6f389
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-16 12:19:24 +0100

    snack 2 completed

---
+++ snack 2/assets/scripts/script.js
@@ -0,0 +1,12 @@
+let word1 = prompt("Inserisci la prima parola");
+let word2 = prompt("Inserisci la seconda parola");
+
+if(word1.length>word2.length){
+    alert(`La parola più lunga è "${word1}". La parola più corta è "${word2}"`);
+}
+else if(word1.length<word2.length){
+    alert(`La parola più lunga è "${word2}". La parola più corta è "${word1}"`);
+}
+else{
+    alert(`Le parole "${word1}" e "${word2}" hanno la stessa lunghezza`);
+}
