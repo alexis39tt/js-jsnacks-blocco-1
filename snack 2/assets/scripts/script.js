ad9790103d74f1ef04818f940888166279273019
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-16 12:37:12 +0100

    snack 3 completed

--- snack 2/assets/scripts/script.js
+++ snack 2/assets/scripts/script.js
@@ -1,10 +1,10 @@
 let word1 = prompt("Inserisci la prima parola");
 let word2 = prompt("Inserisci la seconda parola");
 
-if(word1.length>word2.length){
+if(word1.length > word2.length){
     alert(`La parola più lunga è "${word1}". La parola più corta è "${word2}"`);
 }
-else if(word1.length<word2.length){
+else if(word1.length < word2.length){
     alert(`La parola più lunga è "${word2}". La parola più corta è "${word1}"`);
 }
 else{
---
+++ snack 3/assets/scripts/script.js
@@ -0,0 +1,8 @@
+let somma = 0;
+
+for(i=0; i<10; i++){
+    let num = parseInt(prompt("Inserisci un numero"));
+    somma = somma + num;
+}
+
+alert(`La somma dei numeri è ${somma}`);
