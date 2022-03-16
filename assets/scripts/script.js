c0da9df8d225852b32a3eb48f3c082ca0b683465
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-16 12:02:31 +0100

    added isNaN validation to snack 1

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,17 @@
 let num1 = prompt("Inserisci il primo numero");
 let num2 = prompt("Inserisci il secondo numero");
 
-if(num1>num2){
-    alert(`Il numero più grande è il primo (${num1})`);
+if (isNaN(num1) || isNaN(num2)) {
+    alert("Non hai inserito dei numeri");
 }
-else if (num1<num2){
-    alert(`Il numero più grande è il secondo (${num2})`);
-}
-else{
-    alert("I due numeri sono uguali!");
+else {
+    if (num1 > num2) {
+        alert(`Il numero più grande è il primo (${num1})`);
+    }
+    else if (num1 < num2) {
+        alert(`Il numero più grande è il secondo (${num2})`);
+    }
+    else {
+        alert("I due numeri sono uguali!");
+    }
 }
